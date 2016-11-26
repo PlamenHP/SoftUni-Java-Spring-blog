45bd5dce0b81ed765c469a7b92837cff6a3fc172
PlamenHP <p.petrov.mail@gmail.com>
2016-11-26 18:14:23 +0000

    creating home controller

---
+++ src/main/java/softuniBlog/controller/UserController.java
@@ -0,0 +1,23 @@
+package softuniBlog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import softuniBlog.repository.RoleRepository;
+import softuniBlog.repository.UserRepository;
+
+@Controller
+public class UserController {
+    @Autowired
+    RoleRepository roleRepository;
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("view", "user/register");
+
+        return "base-layout";
+    }
+}
